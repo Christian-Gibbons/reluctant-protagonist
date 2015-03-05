1c0169e2493bb23099e293bb182e76d21223453f
Christian-Gibbons <cgibbons@gmu.edu>
2015-03-05 14:58:32 -0500

    added game timer

--- js/main.js
+++ js/main.js
@@ -36,6 +36,8 @@ window.onload = function() {
 	var playerDirection;
 
 	var protagonist;
+	var protagonistDirection;
+
 	var cursors;
 
 	var RANGE = 50;
@@ -48,6 +50,13 @@ window.onload = function() {
 	var spawnTime = 0;
 
 	var score = 0;
+
+	var style = { font: "30px Verdana", fill: "#ffffff", align: "center" };
+	var gameTime = 120; //2 minutes
+	var timeLeft = gameTime;
+
+	var timeText;
+
 	function create() {
 		map = game.add.tilemap('diamond');
 
@@ -65,10 +74,10 @@ window.onload = function() {
 		player.body.gravity.y = 0;
 		player.body.collideWorldBounds = true;
 
-		player.animations.add('playerWalkRight', [9, 10, 11], 10, true);
-		player.animations.add('playerWalkLeft', [27, 28, 29], 10, true);
-		player.animations.add('playerWalkUp', [0, 1, 2], 10, true);
-		player.animations.add('playerWalkDown', [18, 19, 20], 10, true);
+		player.animations.add('walkRight', [9, 10, 11], 10, true);
+		player.animations.add('walkLeft', [27, 28, 29], 10, true);
+		player.animations.add('walkUp', [0, 1, 2], 10, true);
+		player.animations.add('walkDown', [18, 19, 20], 10, true);
 
 		playerDirection = 'down';
 
@@ -81,7 +90,12 @@ window.onload = function() {
 		protagonist.body.gravity.y = 0;
 		protagonist.body.collideWorldBounds = true;
 
+		protagonist.animations.add('walkRight', [12,13,14], 10, true);
+		protagonist.animations.add('walkLeft', [9,10,11], 10, true);
+		protagonist.animations.add('walkUp', [1,2,3], 10, true);
+		protagonist.animations.add('walkDown', [5,6,7], 10, true);
 
+		protagonistDirection = 'down';
 		createBallSpawn();
 
 		baseballs = game.add.group();
@@ -92,8 +106,14 @@ window.onload = function() {
 		baseballs.setAll('anchor.y', 0.5);
 		baseballs.setAll('outOfBoundsKill', true);
 		baseballs.setAll('checkWorldBounds', true);
+		baseballs.callAll('animations.add', 'animations', 'spinUp', [5,4,3], 10, true);
+		baseballs.callAll('animations.add', 'animations', 'spinDown', [3,4,5], 10, true);
+		baseballs.callAll('animations.add', 'animations', 'spinLeft', [0,1,2], 10, true);
+		baseballs.callAll('animations.add', 'animations', 'spinRight', [2,1,0], 10, true);
 
 		cursors = game.input.keyboard.createCursorKeys();
+
+		timeText = game.add.text( game.world.width/2 , 20, "", style );
 	}
 
 	function update() {
@@ -112,22 +132,22 @@ window.onload = function() {
 		if(cursors.down.isDown){
 			player.body.velocity.y += walkSpeed;
 			playerDirection = 'down';
-			player.animations.play('playerWalkDown');
+			player.animations.play('walkDown');
 		}
 		else if(cursors.up.isDown){
 			player.body.velocity.y -= walkSpeed;
 			playerDirection = 'up';
-			player.animations.play('playerWalkUp');
+			player.animations.play('walkUp');
 		}
 		if(cursors.left.isDown){
 			player.body.velocity.x -= walkSpeed;
 			playerDirection = 'left';
-			player.animations.play('playerWalkLeft');
+			player.animations.play('walkLeft');
 		}
 		else if(cursors.right.isDown){
 			player.body.velocity.x += walkSpeed;
 			playerDirection = 'right';
-			player.animations.play('playerWalkRight');			
+			player.animations.play('walkRight');			
 		}
 		if(!(cursors.down.isDown || cursors.up.isDown || cursors.left.isDown || cursors.right.isDown)){
 			player.animations.stop();
@@ -149,12 +169,46 @@ window.onload = function() {
 		if(protagonistRange.displacement <= RANGE){
 			protagonist.body.velocity.x += (1-(protagonistRange.displacement/RANGE)) * protagonistMaxSpeed * Math.cos(protagonistRange.angle);
 			protagonist.body.velocity.y += (1-(protagonistRange.displacement/RANGE)) * (-1) * protagonistMaxSpeed * Math.sin(protagonistRange.angle);
+			if((protagonistRange.angle >= -Math.PI/4) && (protagonistRange.angle < Math.PI/4)){
+				protagonist.animations.play('walkRight');
+				protagonistDirection = 'right';
+			}
+			else if((protagonistRange.angle >= Math.PI/4) && (protagonistRange.angle < (3*Math.PI/4))){
+				protagonist.animations.play('walkUp');
+				protagonistDirection = 'up';
+			}
+			else if((protagonistRange.angle >= (-3*Math.PI/4)) && (protagonistRange.angle < -Math.PI/4)){
+				protagonist.animations.play('walkDown');
+				protagonistDirection = 'down';
+			}
+			else{
+				protagonist.animations.play('walkLeft');
+				protagonistDirection = 'left';
+			}
+		}
+		else{
+			protagonist.animations.stop();
+			if(protagonistDirection === 'right'){
+				protagonist.frame = 13;
+			}
+			else if(protagonistDirection === 'up'){
+				protagonist.frame = 0;
+			}
+			else if(protagonistDirection === 'down'){
+				protagonist.frame = 4;
+			}
+			else if(protagonistDirection === 'left'){
+				protagonist.frame = 8;
+			}
 		}
 		if(game.time.now >= spawnTime){
 			var spawnPoint = Math.floor(Math.random() * (3.99999));
 			spawnBall(ballSpawns.children[spawnPoint]);
 			spawnTime = game.time.now + 2000;
 		}
+		timeLeft = gameTime - (game.time.now/1000);
+		timeText.text = 'time remaining: ' + timeLeft;
+
 	}
 
 	function findObjectsByType(type, map, layer) {
@@ -204,15 +258,19 @@ window.onload = function() {
 			var pitchAngle;
 			if(ballSpawn.name === 'homePlate'){
 				pitchAngle = Math.PI/2;
+				baseball.animations.play('spinUp');
 			}
 			else if(ballSpawn.name === 'firstBase'){
 				pitchAngle = Math.PI;
+				baseball.animations.play('spinLeft');
 			}
 			else if(ballSpawn.name === 'secondBase'){
 				pitchAngle = -Math.PI/2;
+				baseball.animations.play('spinDown');
 			}
 			else if(ballSpawn.name === 'thirdBase'){
 				pitchAngle = 0;
+				baseball.animations.play('spinRight');
 			}
 			pitchAngle += (.2*Math.random())-.1;
 			ballSpeed -= (Math.floor(50*Math.random()));
