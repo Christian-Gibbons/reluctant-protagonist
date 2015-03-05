5966eb867d9f69ffa0a3d76ba8d86081d5b2a3ab
Christian-Gibbons <cgibbons@gmu.edu>
2015-03-05 03:24:28 -0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,52 +1,70 @@
 window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-    }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
-    }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
-    }
+	// You might want to start with a template that uses GameStates:
+	//     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
+
+	// You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+	// You will need to change the fourth parameter to "new Phaser.Game()" from
+	// 'phaser-example' to 'game', which is the id of the HTML element where we
+	// want the game to go.
+	// The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+	// You will need to change the paths you pass to "game.load.image()" or any other
+	// loading functions to reflect where you are putting the assets.
+	// All loading functions will typically all be found inside "preload()".
+
+	"use strict";
+
+	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+
+	function preload() {
+		game.load.tilemap('diamond', 'assets/tilemaps/maps/diamond.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.image('happylandTiles', 'assets/tilemaps/tilesets/happyland/tileset_8.png');
+		game.load.spritesheet('player', 'assets/sprites/sara/sara\ 16x18\ source.png', 16,18);
+		game.load.spritesheet('protagonist', 'assets/sprites/base\ hero/Hero.png',16,16);
+	}
+	var map;
+	var backgroundLayer;
+//	var fringeLayer;
+//	var collisionLayer;
+
+	var playerStart;
+	var protagonistStart;
+
+	var player;
+	var protagonist;
+
+	function create() {
+		map = game.add.tilemap('diamond');
+
+		map.addTilesetImage('tileset_8', 'happylandTiles');
+
+		backgroundLayer = map.createLayer('Background');
+		backgroundLayer.resizeWorld();
+
+		playerStart = findObjectsByType('playerStart', map, 'Objects');
+		player = game.add.sprite(playerStart[0].x, playerStart[0].y, 'player');
+
+		protagonistStart = findObjectsByType('npcStart', map, 'Objects');
+		protagonist = game.add.sprite(protagonistStart[0].x, protagonistStart[0].y, 'protagonist');
+	}
+
+	function update() {
+	}
+
+	function findObjectsByType(type, map, layer) {
+		var result = new Array();
+		map.objects[layer].forEach(function(element){
+			if(element.properties.type === type){
+				element.y -= map.tileHeight;
+				result.push(element);
+			}
+		});
+		return result;
+	}
+
+	function createFromTiledObject(element, group){
+		var sprite = group.create(element.x, element.y, element.properties.sprite);
+		Object.keys(element.properties).forEach(function(key){
+			sprite[key] = element.properties[key];
+		});
+	}
 };
