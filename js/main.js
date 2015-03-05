b77534886e94fb6db663ed9b0bd169f914c1f85d
Christian-Gibbons <cgibbons@gmu.edu>
2015-03-05 11:14:26 -0500

    get distance and angle between player and protagonist

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ window.onload = function() {
 
 	"use strict";
 
-	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+	var game = new Phaser.Game( 900, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
 
 	function preload() {
 		game.load.tilemap('diamond', 'assets/tilemaps/maps/diamond.json', null, Phaser.Tilemap.TILED_JSON);
@@ -30,7 +30,11 @@ window.onload = function() {
 	var protagonistStart;
 
 	var player;
+	var playerDirection;
+
 	var protagonist;
+
+	var cursors;
 
 	function create() {
 		map = game.add.tilemap('diamond');
@@ -42,12 +46,75 @@ window.onload = function() {
 
 		playerStart = findObjectsByType('playerStart', map, 'Objects');
 		player = game.add.sprite(playerStart[0].x, playerStart[0].y, 'player');
+		player.anchor.setTo(0.5,0.5);
+
+		game.physics.arcade.enable(player);
+		player.body.bounce.y = 0.0;
+		player.body.gravity.y = 0;
+		player.body.collideWorldBounds = true;
+
+		player.animations.add('playerWalkRight', [9, 10, 11], 10, true);
+		player.animations.add('playerWalkLeft', [27, 28, 29], 10, true);
+		player.animations.add('playerWalkUp', [0, 1, 2], 10, true);
+		player.animations.add('playerWalkDown', [18, 19, 20], 10, true);
+
+		playerDirection = 'down';
 
 		protagonistStart = findObjectsByType('npcStart', map, 'Objects');
 		protagonist = game.add.sprite(protagonistStart[0].x, protagonistStart[0].y, 'protagonist');
+		protagonist.anchor.setTo(0.5,0.5);
+
+		game.physics.arcade.enable(protagonist);
+		protagonist.body.bounce.y = 0.0;
+		protagonist.body.gravity.y = 0;
+		protagonist.body.collideWorldBounds = true;
+
+
+		cursors = game.input.keyboard.createCursorKeys();
 	}
 
 	function update() {
+		var protagonistRange = getPolarDifference(player,protagonist);
+		player.body.velocity.x = 0;
+		player.body.velocity.y = 0;
+		var walkSpeed = 75;
+		if(cursors.down.isDown){
+			player.body.velocity.y += walkSpeed;
+			playerDirection = 'down';
+			player.animations.play('playerWalkDown');
+		}
+		else if(cursors.up.isDown){
+			player.body.velocity.y -= walkSpeed;
+			playerDirection = 'up';
+			player.animations.play('playerWalkUp');
+		}
+		if(cursors.left.isDown){
+			player.body.velocity.x -= walkSpeed;
+			playerDirection = 'left';
+			player.animations.play('playerWalkLeft');
+		}
+		else if(cursors.right.isDown){
+			player.body.velocity.x += walkSpeed;
+			playerDirection = 'right';
+			player.animations.play('playerWalkRight');			
+		}
+		if(!(cursors.down.isDown || cursors.up.isDown || cursors.left.isDown || cursors.right.isDown)){
+			player.animations.stop();
+			player.frame = 1;
+			switch(playerDirection){
+				case 'right':
+					player.frame += 9;
+					break;
+				case 'down':
+					player.frame += 18;
+					break;
+				case 'left':
+					player.frame += 27;
+					break;
+				default:
+					break;
+			}
+		}
 	}
 
 	function findObjectsByType(type, map, layer) {
@@ -66,5 +133,25 @@ window.onload = function() {
 		Object.keys(element.properties).forEach(function(key){
 			sprite[key] = element.properties[key];
 		});
+	}
+
+	function createBallSpawn(){
+		ballSpawns = game.add.group();
+		ballSpawns.enableBody = true;
+		var ballSpawn;
+		result = findObjectsByType('ballSpawn', map, 'Objects');
+		result.forEach(function(element){
+			createFromTiledObject(element, ballSpawns);
+		}, this);
+	}
+	function getPolarDifference(object1, object2){
+		var displacement = {};
+		var ret = {};
+		displacement.x = object2.x - object1.x;
+		displacement.y = object1.y - object2.y; //game world exists in quadrant 4
+		displacement.abs = Math.sqrt((displacement.x*displacement.x)+(displacement.y*displacement.y));
+		ret.displacement = displacement.abs;
+		ret.angle = Math.atan2((displacement.y),(displacement.x));
+		return ret;
 	}
 };
